8065a76eb1f8ab080811645f5c2e1ef094ac795f
Mostafa Abayazeed <mostafaabayazeed@yahoo.com>
2024-12-04 00:30:49 +0200

    user

---
+++ ConnectHub/src/main/java/org/example/demo/User.java
@@ -0,0 +1,17 @@
+package org.example.demo;
+
+import java.util.Date;
+
+public class User {
+    private String userID;
+    private String username;
+    private String email;
+    private String password;
+    private Date DOB;
+    private boolean status;
+    // pfp
+    // cover photo
+    // bio
+    // post arraylist
+    // friends arraylist
+}
