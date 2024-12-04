e9f6b6462519f86d2f69339c4f2a2ca65c78055d
Eyad Ahmed <103265469+EyadAhmed01@users.noreply.github.com>
2024-12-04 22:37:18 +0200

    j

--- ConnectHub/src/main/java/org/example/demo/Post.java
+++ ConnectHub/src/main/java/org/example/demo/Post.java
@@ -1,11 +1,21 @@
 package org.example.demo;
 
+import javafx.scene.image.Image;
+
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Post{
     private User owner;
     private String content;
     private String image;
-    private Date datePosted;
-    private int id
+    private LocalDate datePosted;
+    public Post( String content) {
+        this.content = content;
+        this.datePosted = LocalDate.now();
+    }
+    public Post(User owner, Image imageContent) {
+        this.image = imageContent.toString();
+        this.datePosted = LocalDate.now();
+    }
 }
