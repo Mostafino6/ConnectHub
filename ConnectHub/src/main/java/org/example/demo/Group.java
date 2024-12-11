b17b5590df77e92d60b6b95e907584f1b27c4373
Mostafa Abayazeed <mostafaabayazeed@yahoo.com>
2024-12-11 14:28:31 +0200

    group class

---
+++ ConnectHub/src/main/java/org/example/demo/Group.java
@@ -0,0 +1,12 @@
+package org.example.demo;
+
+import java.util.ArrayList;
+
+public class Group {
+    private String groupID;
+    private User creator;
+    private String groupName;
+    private String groupDescription;
+    private String groupIcon;
+    private  HierarchyManagement hierarchy;
+}
---
+++ ConnectHub/src/main/java/org/example/demo/HierarchyManagement.java
@@ -0,0 +1,9 @@
+package org.example.demo;
+
+import java.util.ArrayList;
+
+public class HierarchyManagement {
+    private ArrayList<User> admins;
+    private ArrayList<User> members;
+    private ArrayList<User> requests;
+}
